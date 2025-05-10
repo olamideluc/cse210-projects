8191a432375591f90d589cb0466324a1407b9441
olamideluc <bamigbolaluc@gmail.com>
2025-05-10 13:42:13 +0000

    Finished, week01 Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string vaLueFromUser = Console.ReadLine();
+        int value = int.Parse(vaLueFromUser);
+
+        string letter = "";
+
+        if (value >= 90)
+        {
+            letter = "A";
+        }
+        else if (value >= 80)
+        {
+            letter = "B";
+        }
+        else if (value >= 70)
+        {
+            letter = "C";
+        }
+        else if (value >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (value >= 70)
+        {
+            Console.WriteLine("Congratulation! you passed.");
+        }
+        else
+        {
+            Console.WriteLine("Goodluck, try nextime");
+        }
+    
+    
     }
 }
