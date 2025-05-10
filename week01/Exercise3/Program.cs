aa88af35b66b8cf30574e370035a8c700e28e32d
olamideluc <bamigbolaluc@gmail.com>
2025-05-10 20:36:08 +0000

    Finished Week1 Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is your magic number? ");
+        int MagicNumber = int.Parse(Console.ReadLine());
+        
+
+        // Random randomGenerator = new Random();
+        // int MagicNumber = randomGenerator.Next(1, 101);
+
+        int guess= -1;
+        while (guess != MagicNumber)
+        {
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+
+            if (MagicNumber > guess)
+            {
+
+                Console.WriteLine("Higher");
+            }
+            else if (MagicNumber < guess)
+            {
+
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+
+                Console.WriteLine("You guessed it");
+            }
+
+        }
     }
 }
+        
+        
+        
+        
+        
+        
+        
+            
+        
+        
+        
+        
+        
